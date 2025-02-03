2c7ff943420c82644875449f8db8fa787ccfa6f3
JN <joyng808@gmail.com>
2025-02-03 14:48:42 +0800

    [JN]- Update

--- E-Commerce.API/Program.cs
+++ E-Commerce.API/Program.cs
@@ -1,3 +1,6 @@
+using ECommerceSystem;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ECommerceDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceConnectionStrings"));
+});
+
 
 var app = builder.Build();
 
